63eff3e10d0e0d3187a1ac5ed52ffd309824f853
danielSONCCO <daniel-chen.soncco@360learning.com>
2020-12-05 06:14:48 +0100

    day 5.1

---
+++ day5/solve.py
@@ -0,0 +1,26 @@
+import sys
+
+def solve():
+    encoded_seats = fetch_seats()
+    seat_ids = [get_id(seat) for seat in encoded_seats]
+    return max(seat_ids)
+
+def fetch_seats():
+    lines = read_lines()
+    return [line.strip() for line in lines]
+
+def get_id(seat):
+    raw_row = seat[0:7]
+    raw_col = seat[7:10]
+    binary_row = raw_row.replace("F", "0").replace("B", "1")
+    binary_col = raw_col.replace("L", "0").replace("R", "1")
+    row = int(binary_row, 2)
+    col = int(binary_col, 2)
+    return row * 8 + col
+
+def read_lines():
+    filename = "simple_input.txt" if sys.argv[1] == "simple" else "input.txt"
+    with open(filename, "r") as file:
+        return file.readlines()
+
+print(solve())
