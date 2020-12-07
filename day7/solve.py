f3c1bb74f053614ef5a485fc7207f356c6f3f3d8
danielSONCCO <daniel-chen.soncco@360learning.com>
2020-12-07 06:45:06 +0100

    day 7.1

---
+++ day7/solve.py
@@ -0,0 +1,52 @@
+import sys
+import re
+
+memo_bags = dict()
+
+def solve():
+    bags = fetch_bags()
+    return sum(1 if has_shiny_gold(bag["type"], bags) else 0 for bag in bags) - 1
+
+def fetch_bags():
+    lines = [line.strip() for line in read_lines()]
+    return [build_bag(line) for line in lines]
+
+def build_bag(line):
+    match = re.search("^(.*) bags contain (.*)$", line)
+    if match is None:
+        raise
+    bag_type = match.group(1)
+    raw_content = match.group(2)
+    return dict({ "type": bag_type, "content": build_content(raw_content) })
+
+def build_content(raw_content):
+    if raw_content == "no other bags.":
+        return []
+    raw_bags = raw_content[0:-1].split(", ")
+    return [build_inner_bag(bag) for bag in raw_bags]
+def build_inner_bag(bag):
+    match = re.search("^(\d) (.*) (bag|bags)$", bag)
+    return match.group(2)
+
+def has_shiny_gold(bag_type, bags):
+    if bag_type in memo_bags:
+        return memo_bags[bag_type]
+    bag = next((bag for bag in bags if bag["type"] == bag_type), None)
+    if bag is None:
+        raise
+    if bag_type == "shiny gold":
+        memo_bags[bag_type] = True
+        return True
+    for inner_bag_type in bag["content"]:
+        if has_shiny_gold(inner_bag_type, bags):
+            memo_bags[bag_type] = True
+            return True
+    memo_bags[bag_type] = False
+    return False
+
+def read_lines():
+    filename = "simple_input.txt" if sys.argv[1] == "simple" else "input.txt"
+    with open(filename, "r") as file:
+        return file.readlines()
+
+print(solve())
