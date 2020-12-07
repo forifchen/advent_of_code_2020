8597f318e9131eb25b6add1a5b97875466b35fbf
danielSONCCO <daniel-chen.soncco@360learning.com>
2020-12-07 07:04:14 +0100

    day 7.2

--- day7/solve.py
+++ day7/solve.py
@@ -5,7 +5,7 @@ memo_bags = dict()
 
 def solve():
     bags = fetch_bags()
-    return sum(1 if has_shiny_gold(bag["type"], bags) else 0 for bag in bags) - 1
+    return count_bags("shiny gold", bags) - 1
 
 def fetch_bags():
     lines = [line.strip() for line in read_lines()]
@@ -26,7 +26,7 @@ def build_content(raw_content):
     return [build_inner_bag(bag) for bag in raw_bags]
 def build_inner_bag(bag):
     match = re.search("^(\d) (.*) (bag|bags)$", bag)
-    return match.group(2)
+    return [match.group(2), match.group(1)]
 
 def has_shiny_gold(bag_type, bags):
     if bag_type in memo_bags:
@@ -43,6 +43,18 @@ def has_shiny_gold(bag_type, bags):
             return True
     memo_bags[bag_type] = False
     return False
+
+def count_bags(bag_type, bags):
+    if bag_type in memo_bags:
+        return memo_bags[bag_type]
+    bag = next((bag for bag in bags if bag["type"] == bag_type), None)
+    if bag is None:
+        raise
+    result = 1
+    for inner_bag_type, count in bag["content"]:
+        result += int(count) * count_bags(inner_bag_type, bags)
+    memo_bags[bag_type] = result
+    return result
 
 def read_lines():
     filename = "simple_input.txt" if sys.argv[1] == "simple" else "input.txt"
