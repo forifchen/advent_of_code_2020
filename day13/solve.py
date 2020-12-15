1ca7b2eed72ada43ff0aaff95ddc89c5c7a3ad07
danielSONCCO <daniel-chen.soncco@360learning.com>
2020-12-15 07:29:37 +0100

    day 13.2

--- day13/solve.py
+++ day13/solve.py
@@ -4,19 +4,36 @@ def solve():
     notes = fetch_notes()
     minimal_time = int(notes[0])
     bus_ids = get_ids(notes[1])
-    print(bus_ids)
-    best_id, timestamp = compute_best_id(bus_ids, minimal_time)
-    return best_id * (timestamp - minimal_time)
+    return compute_time(bus_ids)
+
+def get_bezout_decomposition(a, b):
+    if b == 0:
+        return [1, 0]
+    [x, y] = get_bezout_decomposition(b, a % b)
+    return [y, x - y*(a/b)]
+def compute_inverse(a, b):
+    x, _ = get_bezout_decomposition(a, b)
+    return x
+
+def merge_conditions(condition1, condition2):
+    a, x = condition1
+    b, y = condition2
+    k = ((y - x) * compute_inverse(a, b) % b + b) % b
+    return [a*b, (a*k + x) % (a*b)]
 
 def get_ids(line):
     raw_ids = line.split(",")
-    return [int(id) for id in raw_ids if id != "x"]
+    return raw_ids
 
-def compute_best_id(ids, minimal_time):
-    times = [(minimal_time + id - 1) / id * id for id in ids]
-    best_time = min(times)
-    best_id = ids[times.index(best_time)]
-    return [best_id, best_time]
+def compute_time(ids):
+    conditions = []
+    for i in range(len(ids)):
+        if ids[i] == "x":
+            continue
+        number = int(ids[i])
+        conditions.append([number, (-i % number + number) % number])
+    condition = reduce(merge_conditions, conditions[1:], conditions[0])
+    return condition[1]
 
 def fetch_notes():
     lines = read_lines()
