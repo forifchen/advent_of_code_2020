ce012f4aea4976264c892daac450f20c5f015eda
danielSONCCO <daniel-chen.soncco@360learning.com>
2020-12-24 07:28:03 +0100

    day 24.2

--- day24/solve.py
+++ day24/solve.py
@@ -10,7 +10,32 @@ def solve():
             color_tiles.pop(position)
         else:
             color_tiles[position] = True
+    for _ in range(100):
+        color_tiles = run_process(color_tiles)
     return len(color_tiles)
+
+def run_process(color_tiles):
+    neighbor_counter = dict()
+    for tile_key in color_tiles.keys():
+        x, y = [int(number) for number in tile_key.split(",")]
+        neighbors = [(x + 2, y), (x - 2, y), (x + 1, y + 1), (x - 1, y + 1), (x + 1, y - 1), (x - 1, y - 1)]
+        for neighbor in neighbors:
+            neighbor_key = encode(neighbor[0], neighbor[1])
+            if neighbor_key in neighbor_counter:
+                neighbor_counter[neighbor_key] += 1
+            else:
+                neighbor_counter[neighbor_key] = 1
+
+    new_color_tiles = dict()
+    for key in neighbor_counter:
+        if key in color_tiles:
+           if neighbor_counter[key] in [1, 2]:
+               new_color_tiles[key] = True
+        else:
+           if neighbor_counter[key] == 2:
+               new_color_tiles[key] = True
+    return new_color_tiles
+
 
 def get_position(tile):
     x, y = 0, 0
@@ -33,9 +58,10 @@ def get_position(tile):
         if direction == "nw":
             x, y = x - 1, y - 1
             continue
+    return encode(x, y)
+        
+def encode(x, y):
     return str(x) + "," + str(y)
-        
-        
 
 def fetch_tiles():
     lines = read_lines()
